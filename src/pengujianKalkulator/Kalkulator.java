ba50a209f1679b6787dcffded38f2682d7decb78
Septian Isnandar <Septian Isnandar@Isnandar>
2014-05-22 10:28:01 +0700

    first commit

---
+++ src/pengujianKalkulator/Kalkulator.java
@@ -0,0 +1,20 @@
+package pengujianKalkulator;
+
+public class Kalkulator {
+	
+	public double kali(double operandA,double operandB){
+        return operandA*operandB;
+    }
+    
+    public double bagi(double operandA,double operandB){
+        return operandA/operandB;
+    }
+    
+    public double tambah(double operandA,double operandB){
+        return operandA+operandB;
+    }
+    
+    public double kurang(double operandA,double operandB){
+        return operandA-operandB;
+    }   
+}
---
+++ src/pengujianKalkulator/KalkulatorTest.java
@@ -0,0 +1,37 @@
+package pengujianKalkulator;
+
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class KalkulatorTest {
+	
+Kalkulator kalkulator;
+    
+    @Before
+    public void init(){
+        kalkulator=new Kalkulator();
+    }
+ 
+    @Test
+    public void testKali() {
+        assertEquals(20, kalkulator.kali(10, 2),0);
+    }
+ 
+    @Test
+    public void testBagi() {
+        assertEquals(5, kalkulator.bagi(10, 2),0);
+    }
+ 
+    @Test
+    public void testTambah() {
+        assertEquals(12, kalkulator.tambah(10, 2),0);
+    }
+ 
+    @Test
+    public void testKurang() {
+        assertEquals(8, kalkulator.kurang(10, 2),0);
+    }
+
+}
